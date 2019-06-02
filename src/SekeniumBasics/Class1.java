006360432b60becdde203d6d21009324b9b131d1
Mohammad Islam <mislam2248@gmail.com>
2019-06-02 13:41:14 -0400

    master

--- src/SekeniumBasics/Class1.java
+++ src/SekeniumBasics/Class1.java
@@ -1,6 +1,11 @@
 package SekeniumBasics;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -14,6 +19,13 @@ public class Class1 {
 		driver.get("https:\\www.facebook.com");
 //		driver.close();
 		driver.manage().window().maximize();
+		
+		List<WebElement> alllinks= driver.findElements(By.tagName("a"));
+		
+		List<String> txtLinks= new ArrayList<>();
+        for(int i=0;i<alllinks.size();i++) {
+            txtLinks.add(alllinks.get(i).getText());
+            System.out.println(txtLinks.get(0));
 
 		
 //		driver.findElement()
